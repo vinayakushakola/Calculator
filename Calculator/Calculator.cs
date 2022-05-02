2fc698f2f0214fb9a199511dbdc1b9d3b7478601
VinayakUshakola <vinayak.ushakola@gmail.com>
2022-05-02 09:02:06 +0530

    Added: Addition Funtionality

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculator
+{
+    public class Calculator
+    {
+        // Created a Addition method
+        public void Add()
+        {
+            int num = 20;
+            int num2 = 40;
+            int sum = num + num2;
+            Console.WriteLine("Addition of two numbers is {0}", sum);
+        }
+
+        public void Addition(int num1, int num2)
+        {
+            int sum = num1 + num2;
+            Console.WriteLine("Addition of {0} + {1} = {2}", num1, num2, sum);
+        }
+
+        public void Addition2()
+        {
+            Console.WriteLine("Enter 1st number = ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter 2nd number = ");
+            string num = Console.ReadLine();
+            int num2 = Convert.ToInt32(num);
+
+            int sum = num1 + num2;
+            Console.WriteLine("Addition of {0} + {1} = {2}", num1, num2, sum);
+        }
+    }
+}
---
+++ Calculator/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator objCalc = new Calculator();
+
+            Console.WriteLine("Enter 1st number = ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter 2nd number = ");
+            string num = Console.ReadLine();
+            int num2 = Convert.ToInt32(num);
+
+            int sum = num1 + num2;
+            Console.WriteLine("Addition of {0} + {1} = {2}", num1, num2, sum);
+
+            objCalc.Addition(num1, num2);
+        }
+    }
+}
