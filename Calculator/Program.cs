7b3084fb617d84b2833bd1da1942bf35b5e4d9c5
VinayakUshakola <vinayak.ushakola@gmail.com>
2022-05-02 09:14:38 +0530

    Refactored: Program.cs

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -6,17 +6,17 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
+            // Created the objec/instance of calculator class
             Calculator objCalc = new Calculator();
 
+            // Taking inputs from user
             Console.WriteLine("Enter 1st number = ");
             int num1 = Convert.ToInt32(Console.ReadLine());
 
+            // Taking inputs from user
             Console.WriteLine("Enter 2nd number = ");
             string num = Console.ReadLine();
             int num2 = Convert.ToInt32(num);
-
-            int sum = num1 + num2;
-            Console.WriteLine("Addition of {0} + {1} = {2}", num1, num2, sum);
 
             objCalc.Addition(num1, num2);
         }
